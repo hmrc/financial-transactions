0537d28670b7bcaa1016e7bbbff2018a47453112
Lewis Hay <29063729+lewishay@users.noreply.github.com>
2022-11-24 13:55:45 +0000

    Removed unused components for contract test workaround

--- app/testOnly/controllers/ContractTestController.scala
+++ app/testOnly/controllers/ContractTestController.scala
@@ -18,36 +18,15 @@ package testOnly.controllers
 
 import config.MicroserviceAppConfig
 import play.api.mvc.{Action, AnyContent, ControllerComponents}
-import testOnly.connectors.ContractTestConnector
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
-import utils.LoggerUtil
 
 import javax.inject.{Inject, Singleton}
-import scala.concurrent.ExecutionContext
 
 @Singleton
-class ContractTestController @Inject()(connector: ContractTestConnector,
-                                       cc: ControllerComponents,
-                                       appConfig: MicroserviceAppConfig)
-                                      (implicit ec: ExecutionContext) extends BackendController(cc) with LoggerUtil {
+class ContractTestController @Inject()(cc: ControllerComponents,
+                                       appConfig: MicroserviceAppConfig) extends BackendController(cc) {
 
-  def callAPI(url: String): Action[AnyContent] = Action.async { implicit request =>
-    val urlSplit = request.uri.replace("/financial-transactions/test-only", "").split('?')
-
-    val url = urlSplit(0)
-    val queryParams = if(urlSplit.length > 1) {
-      urlSplit(1).split('&').map(param => (param.split('=')(0), param.split('=')(1))).toSeq
-    } else {
-      Seq()
-    }
-
-    connector.callAPI(url, queryParams).map { result =>
-      logger.debug(s"[ContractTestController][callAPI] - Response body received: ${result.body}")
-      Status(result.status)(result.body)
-    }
-  }
-
-  def bearerToken: Action[AnyContent] = Action { implicit request =>
+  def bearerToken: Action[AnyContent] = Action {
     Ok(appConfig.eisToken)
   }
 }
