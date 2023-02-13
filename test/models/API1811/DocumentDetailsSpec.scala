d1a48ee4bb512888c8f25dacb3bd1123becb19e7
Sheila <63787618+SheilaTran@users.noreply.github.com>
2023-02-13 12:36:23 +0000

    BTAT-11325 fix tests

--- test/models/API1811/DocumentDetailsSpec.scala
+++ test/models/API1811/DocumentDetailsSpec.scala
@@ -22,6 +22,33 @@ import utils.API1811.TestConstants._
 
 class DocumentDetailsSpec extends SpecBase {
 
+  "getAccruingPenalty" should {
+
+    "return only the penalty charge that has an accruing penaltyStatus" in {
+
+      val model = fullDocumentDetails.copy(
+        documentPenaltyTotals = Some(Seq(documentPenaltyTotalsPosted, documentPenaltyTotals)))
+
+      model.getAccruingPenalty shouldBe Some(documentPenaltyTotals)
+    }
+
+    "return an empty DocumentPenaltyTotals when PenaltyStatus is not accruing" in {
+
+      val model = fullDocumentDetails.copy(
+        documentPenaltyTotals = Some(Seq(documentPenaltyTotalsPosted)))
+
+      model.getAccruingPenalty shouldBe None
+    }
+
+    "return a none when DocumentPenaltyTotals is empty" in {
+
+      val model = fullDocumentDetails.copy(
+        documentPenaltyTotals = Some(Seq(emptyDocumentPenaltyTotal)))
+
+      model.getAccruingPenalty shouldBe None
+    }
+  }
+
   "DocumentDetails" should {
 
     "read from JSON" when {
@@ -50,65 +77,93 @@ class DocumentDetailsSpec extends SpecBase {
         Json.toJson(emptyDocumentDetails) shouldBe expectedOutput
       }
 
-      "getAccruingPenalty" should {
+      "there is an accruing and a posted penalty charge" in {
 
-        "only write the accruing charge when there is an accruing and a posted penalty" in {
+        val model = fullDocumentDetails.copy(
+          documentPenaltyTotals = Some(Seq(documentPenaltyTotalsPosted, documentPenaltyTotals)))
+        val expectedOutput: JsObject = Json.obj(
+          "chargeType" -> "VAT Return Debit Charge",
+          "periodKey" -> "13RL",
+          "taxPeriodFrom" -> "2017-04-06",
+          "taxPeriodTo" -> "2018-04-05",
+          "chargeReference" -> "XM002610011594",
+          "mainTransaction" -> "4700",
+          "subTransaction" -> "1174",
+          "originalAmount" -> 45552768.79,
+          "outstandingAmount" -> 297873.46,
+          "clearedAmount" -> 45254895.33,
+          "items" -> Json.arr(fullLineItemDetailsOutputJson),
+          "accruingInterestAmount" -> 0.23,
+          "interestRate" -> 3,
+          "accruingPenaltyAmount" -> 10.01,
+          "penaltyType" -> "LPP1"
+        )
+        Json.toJson(model) shouldBe expectedOutput
+      }
 
-          val model = fullDocumentDetails.copy(
-            documentPenaltyTotals = Some(Seq(documentPenaltyTotalsPosted, documentPenaltyTotals)))
-          val expectedOutput: JsObject = Json.obj(
-            "chargeType" -> "VAT Return Debit Charge",
-            "periodKey" -> "13RL",
-            "taxPeriodFrom" -> "2017-04-06",
-            "taxPeriodTo" -> "2018-04-05",
-            "chargeReference" -> "XM002610011594",
-            "mainTransaction" -> "4700",
-            "subTransaction" -> "1174",
-            "originalAmount" -> 45552768.79,
-            "outstandingAmount" -> 297873.46,
-            "clearedAmount" -> 45254895.33,
-            "items" -> Json.arr(fullLineItemDetailsOutputJson),
-            "accruingInterestAmount" -> 0.23,
-            "interestRate" -> 3,
-            "accruingPenaltyAmount" -> 10.01,
-            "penaltyType" -> "LPP1"
-          )
-          Json.toJson(model) shouldBe expectedOutput
-        }
+      "the penaltyStatus is not accruing" in {
 
-        "return no penalty information as penaltyStatus is not accruing and is posted" in {
+        val model = fullDocumentDetails.copy(
+          documentPenaltyTotals = Some(Seq(documentPenaltyTotalsPosted)))
+        val expectedOutput: JsObject = Json.obj(
+          "chargeType" -> "VAT Return Debit Charge",
+          "periodKey" -> "13RL",
+          "taxPeriodFrom" -> "2017-04-06",
+          "taxPeriodTo" -> "2018-04-05",
+          "chargeReference" -> "XM002610011594",
+          "mainTransaction" -> "4700",
+          "subTransaction" -> "1174",
+          "originalAmount" -> 45552768.79,
+          "outstandingAmount" -> 297873.46,
+          "clearedAmount" -> 45254895.33,
+          "items" -> Json.arr(fullLineItemDetailsOutputJson),
+          "accruingInterestAmount" -> 0.23,
+          "interestRate" -> 3
+        )
+        Json.toJson(model) shouldBe expectedOutput
+      }
 
-          val model = fullDocumentDetails.copy(
-            documentPenaltyTotals = Some(Seq(documentPenaltyTotalsPosted)))
-          val expectedOutput: JsObject = Json.obj(
-            "chargeType" -> "VAT Return Debit Charge",
-            "periodKey" -> "13RL",
-            "taxPeriodFrom" -> "2017-04-06",
-            "taxPeriodTo" -> "2018-04-05",
-            "chargeReference" -> "XM002610011594",
-            "mainTransaction" -> "4700",
-            "subTransaction" -> "1174",
-            "originalAmount" -> 45552768.79,
-            "outstandingAmount" -> 297873.46,
-            "clearedAmount" -> 45254895.33,
-            "items" -> Json.arr(fullLineItemDetailsOutputJson),
-            "accruingInterestAmount" -> 0.23,
-            "interestRate" -> 3
-          )
-          Json.toJson(model) shouldBe expectedOutput
-        }
+      "the documentPenaltyTotals array is present but empty" in {
 
-        "return an empty DocumentPenaltyTotals when PenaltyStatus is Posted" in {
+        val model = fullDocumentDetails.copy(
+          documentPenaltyTotals = Some(Seq(emptyDocumentPenaltyTotal)))
+        val expectedOutput: JsObject = Json.obj(
+          "chargeType" -> "VAT Return Debit Charge",
+          "periodKey" -> "13RL",
+          "taxPeriodFrom" -> "2017-04-06",
+          "taxPeriodTo" -> "2018-04-05",
+          "chargeReference" -> "XM002610011594",
+          "mainTransaction" -> "4700",
+          "subTransaction" -> "1174",
+          "originalAmount" -> 45552768.79,
+          "outstandingAmount" -> 297873.46,
+          "clearedAmount" -> 45254895.33,
+          "items" -> Json.arr(fullLineItemDetailsOutputJson),
+          "accruingInterestAmount" -> 0.23,
+          "interestRate" -> 3
+        )
+        Json.toJson(model) shouldBe expectedOutput
+      }
 
-          fullDocumentDetails.copy(
-            documentPenaltyTotals = Some(Seq(documentPenaltyTotalsPosted))).getAccruingPenalty shouldBe None
-        }
+      "the documentPenaltyTotals field is None" in {
 
-        "return the penalty charge when the penaltyStatus is Accruing" in {
-
-          fullDocumentDetails.copy(
-            documentPenaltyTotals = Some(Seq(documentPenaltyTotals))).getAccruingPenalty shouldBe Some(documentPenaltyTotals)
-        }
+        val model = fullDocumentDetails.copy(documentPenaltyTotals = None)
+        val expectedOutput: JsObject = Json.obj(
+          "chargeType" -> "VAT Return Debit Charge",
+          "periodKey" -> "13RL",
+          "taxPeriodFrom" -> "2017-04-06",
+          "taxPeriodTo" -> "2018-04-05",
+          "chargeReference" -> "XM002610011594",
+          "mainTransaction" -> "4700",
+          "subTransaction" -> "1174",
+          "originalAmount" -> 45552768.79,
+          "outstandingAmount" -> 297873.46,
+          "clearedAmount" -> 45254895.33,
+          "items" -> Json.arr(fullLineItemDetailsOutputJson),
+          "accruingInterestAmount" -> 0.23,
+          "interestRate" -> 3
+        )
+        Json.toJson(model) shouldBe expectedOutput
       }
 
       "there are multiple line item details objects" in {
@@ -193,15 +248,15 @@ class DocumentDetailsSpec extends SpecBase {
         Json.toJson(model) shouldBe expectedOutput
       }
     }
-  }
 
-  "throw an exception" when {
+    "throw an exception" when {
 
-    "there are no line item details present" in {
-      val model = fullDocumentDetails.copy(lineItemDetails = Seq())
-      val ex = intercept[JsResultException](Json.toJson(model))
-      ex.errors.head._1 shouldBe JsPath \ "lineItemDetails"
-      ex.errors.head._2 shouldBe List(JsonValidationError(List("Line item details must contain at least 1 item")))
+      "there are no line item details present" in {
+        val model = fullDocumentDetails.copy(lineItemDetails = Seq())
+        val ex = intercept[JsResultException](Json.toJson(model))
+        ex.errors.head._1 shouldBe JsPath \ "lineItemDetails"
+        ex.errors.head._2 shouldBe List(JsonValidationError(List("Line item details must contain at least 1 item")))
+      }
     }
   }
 }
